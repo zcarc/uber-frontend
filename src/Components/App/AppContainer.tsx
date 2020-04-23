2c2d54a5e37d94057935b95ac1a98b602e1e0640
wrkbr <zcarc1@gmail.com>
2020-04-24 05:52:28 +0900

    #2.8 Typescript and Styled Components part Two

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { graphql } from "react-apollo";
-import AppPresenter from './AppPresenter'
+import reset from "styled-reset";
+import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueries";
+import { ThemeProvider } from "src/typed.components";
+import theme from "src/theme";
 
 const AppContainer = (dataParam) => {
   const { data } = dataParam;
-  return <AppPresenter isLoggedIn={data.auth.isLoggedIn} />;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    </ThemeProvider>
+  );
 };
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
