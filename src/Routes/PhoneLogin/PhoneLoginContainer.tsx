6e40bf3289cb6c552f358727583cfaf5a57d84d9
wrkbr <zcarc1@gmail.com>
2020-04-24 17:16:04 +0900

    #2.15 Inputs and Typescript part One

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import PhoneLoginPresenter from "./PhoneLoginPresenter";
+import { RouteComponentProps } from "react-router-dom";
 
-class PhoneLoginContainer extends React.Component {
+interface IState {
+  countryCode: string;
+  phoneNumber: string;
+}
+
+class PhoneLoginContainer extends React.Component<RouteComponentProps<any>, IState> {
+  public state = {
+    countryCode: "+82",
+    phoneNumber: ""
+  };
+
   public render() {
-    return <PhoneLoginPresenter />;
+    const { countryCode, phoneNumber } = this.state;
+    return (
+      <PhoneLoginPresenter
+        countryCode={countryCode}
+        phoneNumber={phoneNumber}
+      />
+    );
   }
 }
 
