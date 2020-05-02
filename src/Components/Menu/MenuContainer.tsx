03121dd4f08e1f7bfd0440d05301e2689952831b
wrkbr <zcarc1@gmail.com>
2020-05-02 22:17:40 +0900

    #2.34 Updating Driver Mode part One

--- src/Components/Menu/MenuContainer.tsx
+++ src/Components/Menu/MenuContainer.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import MenuPresenter from "./MenuPresenter";
-import { Query } from "react-apollo";
-import { userProfile } from "src/types/api";
+import { Query, Mutation } from "react-apollo";
+import { userProfile, toggleDriving } from "src/types/api";
 import { USER_PROFILE } from "src/sharedQueries";
+import { TOGGLE_DRIVING } from "./MenuQueries";
 
 class ProfileQuery extends Query<userProfile> {}
+class ToggleDrivingMutation extends Mutation<toggleDriving> {}
 
 class MenuContainer extends React.Component {
   public render() {
     
     return (
-      <ProfileQuery query={USER_PROFILE}>
-        {({ data, loading }) => <MenuPresenter data={data} loading={loading} />}
-      </ProfileQuery>
+      <ToggleDrivingMutation
+        mutation={TOGGLE_DRIVING}
+        refetchQueries={[{ query: USER_PROFILE }]}
+      >
+        {(toggleDrivingFn) => (
+          <ProfileQuery query={USER_PROFILE}>
+            {({ data, loading }) => (
+              <MenuPresenter
+                data={data}
+                loading={loading}
+                toggleDrivingFn={toggleDrivingFn}
+              />
+            )}
+          </ProfileQuery>
+        )}
+      </ToggleDrivingMutation>
     );
   }
 }
--- src/Components/Menu/MenuPresenter.tsx
+++ src/Components/Menu/MenuPresenter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "../../typed-components";
 import { userProfile } from "src/types/api";
+import { MutationFn } from "react-apollo";
 
 const Container = styled.div`
   height: 100%;
@@ -78,11 +79,13 @@ const ToggleDriving = styled<IToggleProps, any>("button")`
 interface IProps {
   data?: userProfile;
   loading: boolean;
+  toggleDrivingFn: MutationFn;
 }
 
 const MenuPresenter: React.SFC<IProps> = ({
   data: { GetMyProfile: { user = null } = {} } = {},
   loading,
+  toggleDrivingFn,
 }) => (
   <Container>
     {!loading && user && user.fullName && (
@@ -105,7 +108,7 @@ const MenuPresenter: React.SFC<IProps> = ({
         </Header>
         <SLink to="/trips">Your Trips</SLink>
         <SLink to="/settings">Settings</SLink>
-        <ToggleDriving isDriving={user.isDriving}>
+        <ToggleDriving onClick={toggleDrivingFn} isDriving={user.isDriving}>
           {user.isDriving ? "Stop driving" : "Start driving"}
         </ToggleDriving>
       </>
