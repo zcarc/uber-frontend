b33a378126d0c48bcbd768178ac139cd5d5b78cc
wrkbr <zcarc1@gmail.com>
2020-05-05 03:48:19 +0900

    #2.47 Google Maps and Geolocation

--- src/Routes/FindAddress/FindAddressPresenter.tsx
+++ src/Routes/FindAddress/FindAddressPresenter.tsx
@@ -3,11 +3,25 @@ import {Helmet} from 'react-helmet';
 import styled from '../../typed-components';
 
 const Map = styled.div`
-position: absolute;
+  position: absolute;
   top: 0;
   left: 0;
   height: 100%;
   width: 100%;
+  z-index: 1;
+`;
+
+const Center = styled.div`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  z-index: 2;
+  font-size: 30px;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `;
 
 interface IProps {
@@ -22,7 +36,8 @@ class FindAddressPresenter extends React.Component<IProps> {
         <Helmet>
           <title>Find Address | Uber</title>
         </Helmet>
-        <Map innerRef={mapRef} />
+        <Map ref={mapRef} />
+        <Center>📍</Center>
       </div>
     );
   }
